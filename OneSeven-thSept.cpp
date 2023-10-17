8c93192969b624fa6bc473e25c6dd5dc296fcd7d
Rohit Adak <69696532+Rohit24Tech@users.noreply.github.com>
2023-10-17 13:04:33 +0530

    Update OneSeven-thSept.cpp

--- OneSeven-thSept.cpp
+++ OneSeven-thSept.cpp
@@ -128,3 +128,41 @@ Solution:
     return -1;
         
     }
+
+
+Given an array of size N-1 such that it only contains distinct integers in the range of 1 to N. Find the missing element.(GFG)
+
+Example 1:
+
+Input:
+N = 5
+A[] = {1,2,3,5}
+Output: 4
+Example 2:
+
+Input:
+N = 10
+A[] = {6,1,2,8,3,4,7,10,5}
+Output: 9
+
+Your Task :
+You don't need to read input or print anything. Complete the function MissingNumber() that takes array and N as input  parameters and returns the value of the missing number.
+
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+
+
+Constraints:
+1 ≤ N ≤ 106
+1 ≤ A[i] ≤ 106
+
+Solution:
+
+    int missingNumber(vector<int>& array, int n) {
+        // Your code goes here
+        long long int ans = n*(n+1)/2;
+        long long int sum = accumulate(array.begin(),array.end(),0);
+        return ans-sum;
+    }
+
